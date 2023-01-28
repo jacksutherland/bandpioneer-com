b4e33f57a416981fc9f036930688e568b80829e2
Jack Sutherland <jacksutherl@gmail.com>
2023-01-28 17:40:16 -0500

    band image carousel

--- web/js/site.min.js
+++ web/js/site.min.js
@@ -256,5 +256,192 @@ let BandPioneer = {
 		    	setReadingProgress();
 		    }
 		}
+
+		createBandCarousels()
+		{
+			let bands = document.querySelectorAll('.band-carousel');
+
+			BandPioneer.each(bands, function(idx, ele)
+			{
+				new BandPioneer.BandCarousel(ele);
+
+			}.bind(this));
+		}
+	},
+
+	BandCarousel: class
+	{
+		constructor(carousel)
+		{
+			this.scrollSpeed = 0.3;
+
+			this.direction = 'left';
+
+			this.slider = carousel.querySelector('.band-slider');
+
+			this.slides = this.slider.querySelectorAll('.slide');
+
+			this.carouselAnimationRequest = null;
+
+			this.carouselAnimationFrame = null;
+
+			// Randomly rotate images
+
+			BandPioneer.each(this.slides, function(idx, slide)
+			{
+				let min = -6, max = 6;
+				let deg = Math.random() * (max - min) + min;
+				slide.style.transform = 'rotate(' + deg + 'deg)';
+
+			}.bind(this));
+
+			this.addEventListeners();
+
+			this.createAnimationFrame();
+
+		}
+
+		addEventListeners()
+		{
+			this.resize = function()
+			{
+				this.slides = this.slider.querySelectorAll('.slide');
+				let sliderWid = 0;
+
+				BandPioneer.each(this.slides, function(idx, slide)
+				{
+					sliderWid += slide.getBoundingClientRect().width;
+				});
+
+				this.slider.style.width = (sliderWid + 50) + 'px';
+
+			}.bind(this);
+
+			window.addEventListener('resize', function()
+			{
+				this.resize();
+			}.bind(this));
+
+			let startMouseDragEvent = function(event)
+			{
+				let shiftX = (typeof(event.clientX) === 'undefined')
+					? event.touches[0].clientX - this.slider.getBoundingClientRect().x
+					: event.clientX - this.slider.getBoundingClientRect().x;
+
+				this.stopAnimation();
+
+				let mouseMoveEvent = function(event)
+				{
+					let pageX = (typeof(event.pageX) === 'undefined') ? event.touches[0].pageX : event.pageX;
+
+					if(pageX < 0 || pageX > window.innerWidth)
+					{
+						return false;
+					}
+
+					let xPos = pageX - this.slider.offsetWidth / 2 + 'px';
+					let box = this.slider.getBoundingClientRect();
+
+					this.slider.style.transform = 'translateX(' + (pageX - shiftX) + 'px)';
+
+				}.bind(this);
+
+				let mouseUpEvent = function()
+				{
+					document.removeEventListener('mousemove', mouseMoveEvent);
+					document.removeEventListener('touchmove', mouseMoveEvent);
+					document.removeEventListener('mouseup', mouseUpEvent);
+					document.removeEventListener('touchend', mouseUpEvent);
+
+					this.slider.onmouseup = null;
+					this.startAnimation();
+				}.bind(this);
+
+				document.addEventListener('mousemove', mouseMoveEvent);
+				document.addEventListener('touchmove', mouseMoveEvent);
+				document.addEventListener('mouseup', mouseUpEvent);
+				document.addEventListener('touchend', mouseUpEvent);
+
+			}.bind(this);
+
+			this.slider.addEventListener('mousedown', startMouseDragEvent);
+			this.slider.addEventListener('touchstart', startMouseDragEvent);
+
+			this.slider.ondragstart = function()
+			{
+				return false;
+			};
+
+			this.resize();
+		}
+
+		startAnimation = function()
+		{
+			this.carouselAnimationRequest = requestAnimationFrame(this.carouselAnimationFrame);
+		}
+
+		stopAnimation = function()
+		{
+			cancelAnimationFrame(this.carouselAnimationRequest);
+			this.carouselAnimationRequest = null;
+		}
+
+		createAnimationFrame()
+		{
+			this.carouselAnimationFrame = function()
+			{
+				// Occasionally resize to make sure width & coordinate value are still accurate
+
+				if (this.carouselAnimationRequest % 1000 === 0) 
+				{
+					this.resize();
+			        console.log(this.carouselAnimationRequest +' iterations have passed')
+			    }
+
+				let box = this.slider.getBoundingClientRect();
+
+				// console.log('carouselAnimationFrame ' + box.width  + ' < ' + window.innerWidth);
+
+				if(box.width < window.innerWidth)
+				{
+					// Carousel is narrower than screen
+
+					// console.log('carouselAnimationFrame narrower');
+
+					if(box.x <= 0)
+					{
+						this.direction = 'right';
+					}
+					else if((box.width + box.x) > window.innerWidth)
+					{
+						this.direction = 'left';
+					}
+				}
+				else
+				{
+					// Carousel is wider than screen
+
+					// console.log('carouselAnimationFrame wider');
+
+					if(box.x >= 0)
+					{
+						this.direction = 'left';
+					} 
+					else if(box.width - (-1 * box.x) < window.innerWidth)
+					{
+						this.direction = 'right';
+					}
+				}
+
+				this.slider.style.transform = 'translateX(' + (this.direction === 'left' ? (box.x - this.scrollSpeed) : (box.x + this.scrollSpeed)) + 'px)';
+
+				if(this.carouselAnimationRequest !== null)
+				{
+					this.startAnimation();
+				}
+			}.bind(this);
+
+			this.startAnimation();
+		}
 	}
 }
